76a81f4ead8bb18e84874e64ba7da72648d7a2a6
Ethan <ethan2004@gmail.com>
2023-06-17 18:25:44 -0500

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Welcome to react app.
+          Edit <code>src/App.js</code> Bienvenido a mi primera pagina web.
         </p>
         <a
           className="App-link"

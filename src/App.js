f215c7a709eff945debdcb8212fa646942f4a78c
Ethan <ethan2004@gmail.com>
2023-06-21 09:53:50 -0500

    Restaurante

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+function mostrarComidas(region) {
+  var comidasDiv = document.getElementById('comidas');
+  comidasDiv.innerHTML = '';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Bienvenido a mi primera pagina web.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  var comidas = obtenerComidasPorRegion(region);
+
+  if (comidas.length > 0) {
+    var listaComidas = document.createElement('ul');
+
+    for (var i = 0; i < comidas.length; i++) {
+      var comida = document.createElement('li');
+      comida.textContent = comidas[i];
+      listaComidas.appendChild(comida);
+    }
+
+    comidasDiv.appendChild(listaComidas);
+  } else {
+    comidasDiv.textContent = 'No se encontraron comidas para esta región.';
+  }
 }
 
-export default App;
+function obtenerComidasPorRegion(region) {
+  var comidas = [];
 
+  if (region === 'costa') {
+    comidas = ['Ceviche', 'Encebollado', 'Guatita'];
+  } else if (region === 'sierra') {
+    comidas = ['Cuy asado', 'Hornado', 'Llapingachos'];
+  } else if (region === 'amazonia') {
+    comidas = ['Maito de pescado', 'Suri', 'Yuca con chicha'];
+  } else if (region === 'galapagos') {
+    comidas = ['Pescado encocado', 'Lobster', 'Locro de papa'];
+  }
+
+  return comidas;
+}
